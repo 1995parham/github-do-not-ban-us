880fddac574e5ca73b9845e4206d298dde3572b4
Amir Haghighati Maleki <haghighati.amir@gmail.com>
2019-07-26 16:11:44 -0400

    add Amir's message
     * Since the very first day of the internet, those in power in Iran have tried to monitor, control, and even block
     * people's online activities. An ever-growing focus on censorship, filtering, and content moderation has resulted
     * in alternatives that are being used only by those in power inside Iran to spread the word of propaganda among
     * Iranians. These mediums (including, but not limited to: Aparat video service -- a "safe alternative to YouTube,
     * Bale and Soroush messengers -- "safe" alternatives to Telegram and other foreign messengers) are being represented
     * to the people as "better" alternatives since they provide any information, block any unwanted access, and apply
     * any degree of content moderation in favour of those in power. In other words, the act of filtering and censorship
     * is now replaced by content moderation through monitoring and surveillance facilities provided by these alternative
     * services. With a few exceptions, one can say: it is the Iranian surveillance capitalism.
     * 
     * Now GitHub and Microsoft are acting against ordinary Iranian people. The obvious consequence of this discrimination
     * is the unfortunate consensus of people and the developers who live and work inside Iran, that there is a real
     * "threat to Iranian people" by foreign countries.
     * Of course, there are many other online version control systems and tools that are available for Iranian
     * users, but limiting access to the foremost popular tool for Iranian developers would only add to the flames
     * of a false unification of people and government against "the foreign enemy".
     * 
     * There is no so-called "enemy". Instead of making it uglier, human beings must work collectively, not individually
     * or in groups or in specific countries, towards a peaceful and prosperous world. This discrimination, although
     * abiding by the "rule of law", is yet another step towards dehumanization, an act exercised by every single
     * demagogue throughout history. And the bad news is that obviously, this act will also be used by Iranian demagogues
     * within the Iranian propaganda system to justify the Iranian government's disinformation about the "foreign enemy".
     * Just like those in power in Iran, who by "the rule of law" have been discriminating against the Iranian people and are
     * using "libertarian theology" to justify their brutal actions, the U.S. is also spreading the flames of dehumanization
     * in the name of "freedom" and "pursit of happiness". If we do not educate ourselves well enough, this dehumanization
     * will continue until the time comes in which we will only consider "the lack of war" as peace. And this alone, is the
     * biggest trap that we can fall into.
     * 
     * We must not make mistakes; by only writing this, starring, and forking repositories, this act will not be stopped.
     * This discrimination, unfortunately, will go on. But as humans, it is incumbent upon all of us to keep our eyes open
     * so that the ugly world of politics and power will not stop us from stepping towards a united, peaceful, and a
     * prosperous world.

---
+++ together/anweage/main.js
@@ -0,0 +1,39 @@
+/* 
+ *
+ * Since the very first day of the internet, those in power in Iran have tried to monitor, control, and even block
+ * people's online activities. An ever-growing focus on censorship, filtering, and content moderation has resulted
+ * in alternatives that are being used only by those in power inside Iran to spread the word of propaganda among
+ * Iranians. These mediums (including, but not limited to: Aparat video service -- a "safe alternative to YouTube,
+ * Bale and Soroush messengers -- "safe" alternatives to Telegram and other foreign messengers) are being represented
+ * to the people as "better" alternatives since they provide any information, block any unwanted access, and apply
+ * any degree of content moderation in favour of those in power. In other words, the act of filtering and censorship
+ * is now replaced by content moderation through monitoring and surveillance facilities provided by these alternative
+ * services. With a few exceptions, one can say: it is the Iranian surveillance capitalism.
+ * 
+ * Now GitHub and Microsoft are acting against ordinary Iranian people. The obvious consequence of this discrimination
+ * is the unfortunate consensus of people and the developers who live and work inside Iran, that there is a real
+ * "threat to Iranian people" by foreign countries.
+ * Of course, there are many other online version control systems and tools that are available for Iranian
+ * users, but limiting access to the foremost popular tool for Iranian developers would only add to the flames
+ * of a false unification of people and government against "the foreign enemy".
+ * 
+ * There is no so-called "enemy". Instead of making it uglier, human beings must work collectively, not individually
+ * or in groups or in specific countries, towards a peaceful and prosperous world. This discrimination, although
+ * abiding by the "rule of law", is yet another step towards dehumanization, an act exercised by every single
+ * demagogue throughout history. And the bad news is that obviously, this act will also be used by Iranian demagogues
+ * within the Iranian propaganda system to justify the Iranian government's disinformation about the "foreign enemy".
+ * Just like those in power in Iran, who by "the rule of law" have been discriminating against the Iranian people and are
+ * using "libertarian theology" to justify their brutal actions, the U.S. is also spreading the flames of dehumanization
+ * in the name of "freedom" and "pursit of happiness". If we do not educate ourselves well enough, this dehumanization
+ * will continue until the time comes in which we will only consider "the lack of war" as peace. And this alone, is the
+ * biggest trap that we can fall into.
+ * 
+ * We must not make mistakes; by only writing this, starring, and forking repositories, this act will not be stopped.
+ * This discrimination, unfortunately, will go on. But as humans, it is incumbent upon all of us to keep our eyes open
+ * so that the ugly world of politics and power will not stop us from stepping towards a united, peaceful, and a
+ * prosperous world.
+ *
+ */
+console.log('Github is for everyone');
+console.log('OpenSource is for everyone');
+console.log('Education is for everyone');
