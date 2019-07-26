dce59b2e9edf192239b5bcd9febdfb8c97b3e402
Hussein Habibi Juybari <hussein.juybari@gmail.com>
2019-07-26 18:22:16 +0430

    Update main.dart

--- together/husseinhj/main.dart
+++ together/husseinhj/main.dart
@@ -1,6 +1,3 @@
-import 'package:angular/angular.dart';
-import 'package:main/app_component.template.dart' as ng;
-
 // Github is for everyone
 // Do not ban Iranian developers
 // Husseinhj from Iran 🇮🇷
