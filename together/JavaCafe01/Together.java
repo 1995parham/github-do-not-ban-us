a9039304ca156183fee7523be2d5e4115f49853a
Gokul Swaminathan <gokulswamilive@gmail.com>
2019-07-27 15:13:00 -0700

    Added support message

---
+++ together/JavaCafe01/Together.java
@@ -0,0 +1,8 @@
+class Together{
+
+    public static void main (String[] args){
+        String a_message_to_github = "GitHub is for everyone";
+        System.out.println(a_message_to_github);
+    }
+
+}
