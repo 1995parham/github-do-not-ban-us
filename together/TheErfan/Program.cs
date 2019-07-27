56ff788397e43c7ec89eca9e3bd7e815253fb392
Erfan Abedi <11455681+theerfan@users.noreply.github.com>
2019-07-27 20:52:21 +0430

    Update Program.cs

--- together/TheErfan/Program.cs
+++ together/TheErfan/Program.cs
@@ -5,8 +5,7 @@ namespace GitHubDontBanUs
     {
         static void Main() 
         {
-            Console.WriteLine("Banning Iranian developers from the open source world is in the very least against Microsoft's own terms of conduct,");
-            Console.WriteLine("Let alone basic human morals. Stop pushing U.S government's agenda. Seriously.");
+            Console.WriteLine("Github is for everyone.");
         }
     }
 }
