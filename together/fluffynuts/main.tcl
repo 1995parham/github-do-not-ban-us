e10c1a600f03b0e1ad42c8f25fa906cee18ed20f
Davyd McColl <davydm@gmail.com>
2019-07-27 08:28:33 +0200

    :globe_with_meridians: GitHub is for everyone

---
+++ together/fluffynuts/main.tcl
@@ -0,0 +1,7 @@
+#!/bin/env tclsh
+# GitHub is for everyone
+# Banning people based on their place of birth runs counter to the very heart of open-source
+# This action by GitHub, whilst under the pressure of dubious government, has been implemented
+#   in a manner which disappoints me to the very core
+
+puts "GitHub is for EVERYONE"
