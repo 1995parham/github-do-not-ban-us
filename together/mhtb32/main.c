5a57f944ee0fdcd64ac91a5fec0a7ea0767143f3
MHTB <mhtb32@gmail.com>
2019-07-27 11:38:20 +0430

    mhtb32 folder added

---
+++ together/mhtb32/main.c
@@ -0,0 +1,11 @@
+/*
+    * By Mohammad Hussein Tavakoli Bina
+    * To condemn ban of Iranian open source community from Github
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("Github is for Everyone!");
+    return 0;
+}
