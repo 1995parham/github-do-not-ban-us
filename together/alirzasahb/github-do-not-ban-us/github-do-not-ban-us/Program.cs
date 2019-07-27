1627660b97d29fec9fa899f391261c3af638356b
alirzasahb <alireza.sahebi7@gmail.com>
2019-07-27 11:31:37 +0430

    Github Do Not Ban Us In C#, alirzasahb

---
+++ together/alirzasahb/github-do-not-ban-us/github-do-not-ban-us/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace github_do_not_ban_us
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github Do Not Ban Us!");
+            Console.ReadLine();
+        }
+    }
+}
