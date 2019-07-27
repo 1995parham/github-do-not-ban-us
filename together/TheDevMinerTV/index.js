666fa989f12803886ae715e48ada56b9627edabb
Tobias aka. TheDevMinerTV <tobigames200@gmail.com>
2019-07-27 20:55:33 +0200

    Max out the GitHub API

--- together/TheDevMinerTV/index.js
+++ together/TheDevMinerTV/index.js
@@ -5,8 +5,8 @@
 GitHub is for everyone!
 ❤️️ to everyone of you ${data.forks} people!
 
-The newest 20 forks:`))
-  .then(() => require('node-fetch')('https://api.github.com/repos/1995parham/github-do-not-ban-us/forks'))
+The newest 100 forks:`))
+  .then(() => require('node-fetch')('https://api.github.com/repos/1995parham/github-do-not-ban-us/forks?per_page=100'))
   .then(data => data.json())
   .then(data => data.forEach(repo => console.log(`- ${repo.owner.login}`)))
 })()
