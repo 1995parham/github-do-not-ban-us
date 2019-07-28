6d5142c5b15221f51c4fc60c8eacda9547d5a0b5
Rebwar Hosein Poori <rebwar.poori@gmail.com>
2019-07-28 08:01:52 +0430

    Add files via upload

---
+++ together/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GithHubForEveryOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Github is for everyone");
+        }
+    }
+}
