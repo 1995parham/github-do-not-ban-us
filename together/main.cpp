6de966372cd71e6a399590cf06a6a8ac12a95862
junbo <33727211+Adhders@users.noreply.github.com>
2019-07-28 16:46:41 +0800

    Add files via upload

---
+++ together/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "GitHub is for everyone" << std::endl;
+  std::cout << "GitHub is for everyone" << std::endl;
+  std::cout << "GitHub is for everyone" << std::endl;
+  getchar();
+  return 0;
+}
