87746fb98840ba12f9f086f6d85dfffb049b7d52
Sepideh Abadpour <abadpurs@gmail.com>
2019-07-29 15:58:07 +0430

    Update Sepideh Abadpou's message

--- together/SepidehAbadpour/main.cpp
+++ together/SepidehAbadpour/main.cpp
@@ -4,6 +4,7 @@
 int main()
 {
 	std::cout << "GitHub is for everyone" << std::endl;
+	std::cout << "At least do not ban us from using Github Education and GitHub Free since January 2019!" << std::endl;
 	return 0;
 }
 
