cc1f9777d4b5e1c272edf97b894eb36dac077a31
Mahdi Dehestani <mahdi.section8@gmail.com>
2019-07-29 22:56:03 +0430

    Create RMDsection8.py
    GitHub is for everyone

---
+++ together/RMDsection8.py
@@ -0,0 +1,5 @@
+#dont ban us its a mistake - this is not what the big man's whant
+#dont make this game of goverments like u.s this is open source not game
+#you are more powerful than taking command from them #we_are_programmers
+print("GitHub is for everyone")
+#as easy as they dont know #u.s_goverment
