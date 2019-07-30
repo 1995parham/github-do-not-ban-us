f5d6b21010ff32f3da10e3e6dfea44d11ff8e2e8
Josh Katofsky <joshua.katofsky@mail.mcgill.ca>
2019-07-30 12:32:22 -0400

    cleaned up file

--- together/jkatofsky/github_for_everyone.asm
+++ together/jkatofsky/github_for_everyone.asm
@@ -5,11 +5,11 @@ global    _main
           extern    _puts
 
           section   .text
-_main:    push      rbx                     ; Call stack must be aligned
-          lea       rdi, [rel message]      ; First argument is address of message
-          call      _puts                   ; puts(message)
-          pop       rbx                     ; Fix up stack before returning
+_main:    push      rbx
+          lea       rdi, [rel message]
+          call      _puts
+          pop       rbx
           ret
 
           section   .data
-message:  db        "GitHub is for everyone!", 0        ; C strings need a zero byte at the end
+message:  db        "GitHub is for everyone!", 0
