a042c6bac9cffb8b90ae74398c4872f60a8f3fd7
Amir Mohammad Karimi <35371206+AMK9978@users.noreply.github.com>
2019-07-31 11:40:44 +0430

    Github don't ban us

---
+++ together/AMK/Github.java
@@ -0,0 +1,10 @@
+/**
+ * Created by AMK on 7/31/2019.
+ * Life is nice :)
+ * Enjoy coding :D
+ */
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("GitHub is for everyone");
+    }
+}
