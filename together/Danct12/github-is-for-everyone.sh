23405a03321b93a0a7ba5efeb81bb471b71ceb04
Danct12 <danct12@disroot.org>
2019-07-31 07:40:58 +0700

    [info] message to github

---
+++ together/Danct12/github-is-for-everyone.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "GitHub is for everyone, everyone should be able to contribute to open source projects."
+echo "If one day, the US government came and ban us, then the open source community will be ruined."
+
+echo "Also, Huong Tram is my favorite singer."
+
+exit 0
